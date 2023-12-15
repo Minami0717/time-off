9675e65e707738642560123f6e4d37950e6d7619
Minami <dft2007@naver.com>
2023-12-16 00:35:40 +0900

    주소 api 추가

--- src/main/java/com/minami/timeoff/event/EventController.java
+++ src/main/java/com/minami/timeoff/event/EventController.java
@@ -1,6 +1,5 @@
 package com.minami.timeoff.event;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.minami.timeoff.event.model.EventDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -9,11 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("event")
+@RequestMapping("api/event")
 public class EventController {
     private final EventService service;
 
